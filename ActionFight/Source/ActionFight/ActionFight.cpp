97922821f7303ff6a6ee88270ec71d63dd1ecec8
B477042 <37619106+B477042@users.noreply.github.com>
2021-10-16 15:02:55 +0900

    init project

---
+++ ActionFight/Source/ActionFight/ActionFight.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ActionFight.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ActionFight, "ActionFight" );
---
+++ ActionFight/Source/ActionFight/ActionFightGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ActionFightGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONFIGHT_API AActionFightGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
